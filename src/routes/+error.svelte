fc85f4b64fa70f8c081564aa7029bd51767ee7d6
ChenZhoYu <790348264@qq.com>
2024-06-24 16:52:24 +0800

    feat: 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex items-center justify-center text-center" style="height: calc(100vh - 3.6rem)">
+	<h1 class="font-bold lg:text-2xl">{$page.status}: {$page?.error?.message}</h1>
+</div>
